90091f9b10b27494eb7e582c00a38601ef123678
stevenht19 <mstvdev19@gmail.com>
2023-10-11 01:06:11 -0500

    add: backend model validation

--- api_3716/src/main/java/pe/isil/api_3716/model/Curso.java
+++ api_3716/src/main/java/pe/isil/api_3716/model/Curso.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import java.time.LocalTime;
 
@@ -20,8 +22,11 @@ public class Curso {
     private Integer id;
 
     private Integer codigo;
+
     private String nombre;
+
     private Integer creditos;
+
     private LocalTime horas;
 
 }
--- api_3716/src/main/java/pe/isil/api_3716/model/Estudiante.java
+++ api_3716/src/main/java/pe/isil/api_3716/model/Estudiante.java
@@ -7,6 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -16,10 +20,25 @@ public class Estudiante {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotNull
+    @NotBlank
+    @Size(min = 2, max = 50)
     private String nombres;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 2, max = 50)
     private String apellidos;
+
+    @NotNull
+    @Size(min = 8, max = 8)
     private String dni;
+
+    @Email
     private String correo;
+
+    @NotNull
+    @NotBlank
     private Integer id_carrera;
 
 }
